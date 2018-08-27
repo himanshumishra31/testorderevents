f30cd1f93a7c7c8fe3bede5fed3d6cc0526196e1
Himanshu <himanshumishra3@gmail.com>
2018-08-27 13:40:53 +0530

    initial commit

---
+++ db/migrate/20180821072342_create_spree_order_events_tracker.spree_order_events_tracker.rb
@@ -0,0 +1,18 @@
+# This migration comes from spree_order_events_tracker (originally 20180821065113)
+class CreateSpreeOrderEventsTracker < ActiveRecord::Migration[4.2]
+  def change
+    create_table :spree_order_events_trackers do |t|
+      t.text :entity_errors
+      t.text :flash
+      t.text :request_parameters
+      t.text :updated_parameters
+      t.string :ip_address
+      t.string :entity_type
+      t.integer :response_code
+      t.references :user
+      t.references :order
+
+      t.timestamps null: false
+    end
+  end
+end
